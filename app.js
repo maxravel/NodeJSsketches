ef63774265cc1ab8b23a5ace86aac07d04fc148a
maxravel <maxmaterniak@gmail.com>
2018-04-07 09:37:31 +0200

    global objects

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log('hey andrzej!');
+// run in node -> 'node <file name>' in terminal
+
+// ****************************GLOBAL OBJECT
+// console.log('global name');
+
+// setTimeout(function(){
+//     console.log('showing after 3000 ms')
+// }, 3000);
+
+// let time = 0;
+// setInterval(function(){
+//     console.log(time);
+//     time++;
+// }, 1000)
+
+//stop process in terminal by ctrl+s, or by clear interval
+// let time = 0;
+// let timer = setInterval(function(){
+//     console.log(time);
+//     time++;
+//     if(time===6){
+//         clearInterval(timer);
+//     }
+// }, 1000)
+
+//showing path to directory
+// console.log(__dirname);
+
+//showing path directory
+//console.log(__filename);
