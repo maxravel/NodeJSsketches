66598e5d58aa47276860cd0401ffe97b1555b4c0
maxravel <maxmaterniak@gmail.com>
2018-04-08 13:16:19 +0200

    files, directories

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // console.log('hey andrzej!');
 // run in node -> 'node <file name>' in terminal
 
+
 // ****************************GLOBAL OBJECT
 // console.log('global name');
 
@@ -25,7 +26,124 @@
 // }, 1000)
 
 //showing path to directory
-// console.log(__dirname);
+// console.log(__dirname); //double underscore
 
 //showing path directory
 //console.log(__filename);
+
+//normal function statement
+// function sayHi(){
+//     console.log("Hi!");
+// }
+
+// sayHi();
+
+//function expression
+// var sayBye = function(){
+//     console.log('Bye!');
+// }
+
+// sayBye();
+
+// function callFunction(fun){
+//     fun();
+// }
+
+// callFunction(sayBye);
+
+
+//************************************MODULES
+//requires counter.js
+// var stuff = require('./stuff');
+
+// console.log(stuff.counter(['abc','ffd','dfsfs']));
+// console.log(stuff.adder(2,3));
+// console.log(stuff.adder(5,stuff.pi));
+
+
+
+//**************************EVENT EMMITER
+// var events = require('events');
+
+// //EXAMPLE 1
+// //constructor
+// var myEmitter = new events.EventEmitter();
+
+// //event listener
+// myEmitter.on('someEvent', function(mssg){
+//     console.log(mssg);
+// })
+
+// //emitting event
+// myEmitter.emit('someEvent', 'the event was emitted');
+
+
+//EXAMPLE 2
+// var util = require('util');
+// var Person = function(name){
+//     this.name=name;
+// };
+
+// util.inherits(Person, events.EventEmitter);
+
+// var james = new Person('james');
+// var mary = new Person('mary');
+// var riko = new Person('riko');
+
+// var people = [james, mary, riko];
+
+// people.forEach(function(person){
+//     person.on('speak', function(msg){
+//         console.log(person.name + ' said '+ msg);
+//     });
+// });
+
+// james.emit('speak', 'hey dudes');
+
+
+
+//************************************FILE
+// var fs = require('fs');
+
+//Sync read file
+// //read file
+// // var fileA = fs.readFileSync('fileA.txt', 'UTF-8');
+// // console.log(fileA);
+
+//Async readFile
+// fs.readFile('fileA.txt', 'utf8', function(error, data){
+//     console.log(data);
+//     fs.writeFile('writeB.txt', data);
+// })
+
+// console.log('test, code above is async, and this text will be upper in console tha above function resposne')
+
+//async write file
+// //write file
+// // fs.writeFileSync('writeA.txt', fileA);
+
+
+
+//**********************************Making directories
+var fs = require('fs');
+
+//deleting writeA.txt
+// fs.unlink('writeA.txt');
+
+//making directory sync
+// fs.mkdirSync('stuff');
+
+//removing directory
+// fs.rmdirSync('stuff');
+
+//making directory async
+// fs.mkdir('stuff', function(){
+//     fs.readFile('fileA.txt', 'utf8', function(err, data){
+//         fs.writeFile('./stuff/newFileInStuffDir.txt', data);
+//     });
+// });
+
+//removing directory with file inside
+fs.unlink('./stuff/newFileInStuffDir.txt', function(){
+    fs.rmdir('stuff');
+});
---
+++ stuff.js
@@ -0,0 +1,36 @@
+//***************************************MODULES
+
+// var counter = function(arr){
+//     return 'this array have '+arr.length+' elements';
+// }
+
+// var adder = function(a,b){
+//     return `sum of numbers is ${a+b}`;
+// }
+
+// var pi = 3.14;
+
+// module.exports = {
+//     counter:counter,
+//     adder:adder,
+//     pi:pi
+// }
+
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+// module.exports.counter = function(arr){
+//     return 'this array have '+arr.length+' elements';
+// }
+
+// module.exports.adder = function(a,b){
+//     return `sum of numbers is ${a+b}`;
+// }
+
+// module.exports.pi = 3.14;
+
+
+
+//****************************************8
+
