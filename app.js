ab07dd9799a565e677d323a7d9829e8a1e5d76cd
maxravel <maxmaterniak@gmail.com>
2018-04-17 21:55:50 +0200

    nodemon

--- app.js
+++ app.js
@@ -272,7 +272,13 @@ var server = http.createServer(function(req, res){
 
         res.end(JSON.stringify(myObj));
     }
+    else{
+        res.writeHead(404,{'Content-type':'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 server.listen(3000,'127.0.0.1');
 console.log('now listening to port 3000');
+
+//starting nodemon: nodemon app.js    -  nodemon constant upradging saved files
